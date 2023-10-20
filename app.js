7dda9e7a377401ee8ee8941043576375823abc21
mac <a12@Shahils-MacBook-Pro.local>
2023-10-20 17:29:10 +0530

    “anything”

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // Iteration 1: Make the Play button functional.
 // Description: When the Play button is clicked the game page should be displayed.
+
+const playbutton = document.getElementById("play-button");
+
+playbutton.addEventListener('click', function() {
+    window.location.href = "game.html";
+});
--- game.js
+++ game.js
@@ -1,5 +1,66 @@
 // Iteration 2: Generate 2 random number and display it on the screen
 
+const num1 = Math.floor(Math.random() * 100) + 1;
+const num2 = Math.floor(Math.random() * 100) + 1;
+
+const button1 = document.getElementById('number1');
+const button2 = document.getElementById('number2');
+
+button1.innerText = num1;
+button2.innerText = num2;
+
+var score = parseInt(sessionStorage.getItem("score")) || 0
+
 // Iteration 3: Make the options button functional
+const button = document.getElementById("buttons");
+
+button.addEventListener('click', function (event) {
+    if (num1 > num2 && event.target.id == 'greater-than') {
+        console.log('Won');
+        score += 5;
+        window.location.reload()
+        sessionStorage.setItem("score",score);
+
+  
+
+
+    } else if (num2 == num1 && event.target.id == 'equal-to') {
+        console.log('Won');
+        score += 5;
+        window.location.reload()
+        sessionStorage.setItem("score",score);
+
+
+    } else if (num2 > num1 && event.target.id == 'lesser-than') {
+        console.log("Won");
+        score += 5;
+        window.location.reload()
+        sessionStorage.setItem("score",score);
+
+
+    } else {
+        window.location.href = "gameover.html";
+    }
+})
+console.log(score);
 
 // Iteration 4: Build a timer for the game
+
+const timer = document.getElementById('timer');
+let countDown = 5;
+
+timer.textContent = `${countDown}`;
+const timeInterval = setInterval(()=> {
+    countDown--;
+    timer.innerText=`${countDown}`;
+
+    if(countDown==0) {
+        clearInterval(timeInterval);
+        window.location.href = "gameover.html";
+
+    }
+},1000);
+
+// var score = 5;
+// localStorage.setItem("score", score);
+// console.log(localStorage);
